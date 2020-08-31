432b838040023ccda05ae8595da60025c7193aae
Jason Kim <95jasonkim@gmail.com>
2020-08-31 10:16:11 -0800

    Lab1 code

---
+++ lab1/Lab1/lab1.cpp
@@ -0,0 +1,16 @@
+/*
+Cs201 Lab1 Jason Kim
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+	cout << "Hello world" << endl;
+	cout << "The multiple of 33 and 66 is " << 33 * 66 << endl;
+	
+	return 0;
+}
