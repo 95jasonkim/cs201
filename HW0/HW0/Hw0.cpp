ee09c89c931d9823f3c6f4acd6dc817b1f7c722f
Jason Kim <95jasonkim@gmail.com>
2020-09-01 17:29:54 -0800

    Added code to print excerpt from the poem "No man is an Island"

--- HW0/HW0/Hw0.cpp
+++ HW0/HW0/Hw0.cpp
@@ -13,7 +13,9 @@ int main()
 	for (int i = 0; i < 5; i++) {
 		cout << "Hello World" << endl;
 	}
-
+	cout << "\n" << endl;
+	cout << "From \"No Man is an Island\" by John Donne\n" << endl;
+	cout << "No man is an island,\nEntire of itself,\nEvery man is a piece of the continent,\nA part of the main." << endl;
 
 	return 0;
 }
