7392d76629aefbf090d7ad8d7a43c1f818ca95dd
Jason Kim <95jasonkim@gmail.com>
2020-09-01 17:08:57 -0800

    wrote Hw0 main program code for printing Hello World 5 times.

---
+++ HW0/HW0/Hw0.cpp
@@ -0,0 +1,19 @@
+/*
+Jason Kim
+Hw0 CS201
+9/1/2020
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	for (int i = 0; i < 5; i++) {
+		cout << "Hello World" << endl;
+	}
+
+
+	return 0;
+}
