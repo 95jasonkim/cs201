7b2ceb072e507192ea452ba0ba69d085bbe97f5f
Jason Kim <95jasonkim@gmail.com>
2020-09-11 20:06:47 -0800

    Added code for mileskm.cpp converting miles to kilometers

--- HW1/HW1-4/mileskm.cpp
+++ HW1/HW1-4/mileskm.cpp
@@ -3,3 +3,21 @@ Jason Kim
 CS201 HW1 mileskm.cpp
 9/11/2020
 */
+#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+	float m;
+	float k;
+	cout << "Enter miles to be converted to kilometers: ";
+	cin >> m;
+	k = m * 1.609;
+	cout << std::setprecision(3) << std::fixed;
+	cout << m << " miles is equal to " << k << " kilometers.";
+	
+	return 0;
+}
