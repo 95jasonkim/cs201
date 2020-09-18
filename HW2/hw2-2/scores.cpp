b8114eb7319626a15ded0429459fc20e89624832
Jason Kim <95jasonkim@gmail.com>
2020-09-18 03:16:01 -0800

    Added function that inputs names and scores until requirement is met to break

--- HW2/hw2-2/scores.cpp
+++ HW2/hw2-2/scores.cpp
@@ -24,11 +24,35 @@ bool SearchName(const string& nameToFind, const vector<string>& names)
 	else return false;
 }
 
+void InputScores(vector<string>& names, vector<int>& scores)
+{
+	cout << "Enter names and the corresponding scores(Enter noname and 0 to finish)" << endl;
+	while (true)
+	{
+		string name;
+		int score;
+		cout << "Enter the name: ";
+		getline(cin, name);
+		if (SearchName(name, names) == 0)
+		{
+			cout << "ERROR: name already exists." << endl;
+			break;
+		}
+		cout << "Enter the score: ";
+		cin >> score;
+		if (name == "noname" && score == 0)break;
+		names.push_back(name);
+		scores.push_back(score);
+
+	}
+}
+
 
 int main()
 {
 	vector<string> names;
 	vector<int> scores;
+	InputScores(names,scores);
 
 	return 0;
 }
