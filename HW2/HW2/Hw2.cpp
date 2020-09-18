4c141cf9ed9dabd5f9b72ee51c14413a548e06ff
Jason Kim <95jasonkim@gmail.com>
2020-09-18 01:39:00 -0800

    Added InputNames function that inputs 10 names to vector<string> names

--- HW2/HW2/Hw2.cpp
+++ HW2/HW2/Hw2.cpp
@@ -3,3 +3,38 @@ Jason Kim
 CS201 HW2 names.cpp
 9/18/2020
 */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+using std::getline;
+using std::find;
+
+
+void InputNames(vector<string>& names)
+{
+	
+	for (int i = 0; i < 10; i++)
+	{
+		string name;
+		cout << "Please enter a name: ";
+		getline(cin, name);
+		names.push_back(name);
+	}
+}
+
+
+int main(int argc, char **argv)
+{
+	vector<string> names;
+	string nameToFind;
+	InputNames(names);
+	
+	DoesNameExist(nameToFind, names);
+	return 0;
+}
