9c4ee4cdf5379eb03abbe58732252dd7b8126425
Jason Kim <95jasonkim@gmail.com>
2020-09-24 00:21:30 -0800

    added part 1 for lab 10

--- labs/lab10/lab10.cpp
+++ labs/lab10/lab10.cpp
@@ -3,3 +3,22 @@ Jason Kim
 CS201 lab10
 9/23/2020
 */
+#include"lab10.hpp"
+
+bool isDiv3(int n)
+{
+	if (n % 3 == 0) return true;
+	else return false;
+}
+
+int main()
+{
+	for (int i = 1; i <= 40; i++)
+	{
+		if (isDiv3(i))
+		{
+			cout << i << " is divisble by 3." << endl;
+		}
+	}
+	return 0;
+}
---
+++ labs/lab10/lab10.hpp
@@ -0,0 +1,15 @@
+/*
+Jason Kim
+CS201 lab10.hpp
+9/23/2020
+*/
+#ifndef LAB_TEN
+#define LAB_TEN
+#include<iostream>
+using std::cout;
+using std::endl;
+
+//takes int n and returns true if it is divisible by 3
+bool isDiv3(int n);
+
+#endif
