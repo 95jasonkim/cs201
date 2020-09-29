e7828aff0ec5492aa1b82e517ed36bad300de8f2
Jason Kim <95jasonkim@gmail.com>
2020-09-29 04:23:38 -0800

    Added function boxer, prints the word boxed by layer of asterisks

--- HW3/HW3_MainProgram/boxer.cpp
+++ HW3/HW3_MainProgram/boxer.cpp
@@ -7,8 +7,19 @@ CS 201 boxer.cpp
 
 void boxer(string word, int box)
 {
-
-
+	for (int i = 1; i <= box; i++)
+	{
+		cout << ast(word.size() + 2 + 2 * box) << endl;
+	}
+	string space;
+	space.resize(word.size() + 2, ' ');
+	cout << ast(box) << space << ast(box) << endl;
+	cout << ast(box) << " " << word << " " << ast(box) << endl;
+	cout << ast(box) << space << ast(box)<<endl;
+	for (int i = 1; i <= box; i++)
+	{
+		cout << ast(word.size() + 2 + 2 * box)<<endl;
+	}
 }
 
 string ast(int n)
