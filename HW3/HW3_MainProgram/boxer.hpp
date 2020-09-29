91102e14dc81002f4c150c13151267443b2b0d6f
Jason Kim <95jasonkim@gmail.com>
2020-09-29 04:04:53 -0800

    Added code for inputting string and integer including breaking the loop and checking for positive integer

--- HW3/HW3_MainProgram/boxer.hpp
+++ HW3/HW3_MainProgram/boxer.hpp
@@ -3,9 +3,15 @@ Jason Kim
 CS 201 boxer.hpp
 9/28/2020
 */
-
 #ifndef BOXER_HPP
 #define BOXER_HPP
+#include<iostream>
+using std::endl;
+using std::cout;
+using std::cin;
+#include<string>
+using std::string;
+using std::stoi;
 
 
 #endif
--- HW3/HW3_MainProgram/boxprint.cpp
+++ HW3/HW3_MainProgram/boxprint.cpp
@@ -4,3 +4,31 @@ CS 201 boxprint.cpp
 9/28/2020
 */
 #include"boxer.hpp"
+
+int main()
+{
+	cout << "The word you type will be boxed in layers of asterisks." << endl;
+	while (true)
+	{
+		string word;
+		cout << "Enter your word(enter end to finish): ";
+		getline(cin, word);
+		if (word == "end")break;
+		int box=0;
+		while (box < 1)
+		{
+			cout << "Enter the size of the box: ";
+			string boxs;
+			getline(cin, boxs);
+			box = stoi(boxs);
+			if (box < 1)
+			{
+				cout << "Please enter a positive integer." << endl;
+			}
+		}
+
+		//cout << "word: " << word << " box: " << box;
+	}
+
+	return 0;
+}
