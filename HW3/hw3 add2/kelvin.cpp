6166c26f2fa807c33ff25e10bba33fc08520f8f4
Jason Kim <95jasonkim@gmail.com>
2020-09-29 05:01:39 -0800

    Added errors found in comments

--- HW3/hw3 add2/kelvin.cpp
+++ HW3/hw3 add2/kelvin.cpp
@@ -8,12 +8,29 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+/*original ctok function
+double ctok(double c)
+{
+	int k = c + 273.15; // k needs to be a double as well or the .15 will be excluded
+	return int; // is not returning anything, should return k
+}
+*/
 
 double ctok(double c)
 {
 	double k = c + 273.15;
 	return k;
 }
+
+/*original main function
+int main()
+{
+	double c = 0; // no need to set c as 0
+	cin >> d; // is placing in d instead of c
+	double k = ctok("c"); // no "" needed
+	Cout << k << '/n'; // cout should not be capitalized and line break is \n
+}
+*/
 
 int main()
 {
