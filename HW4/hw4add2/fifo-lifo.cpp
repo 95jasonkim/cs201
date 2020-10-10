b946f5c49d27edc213aa816fd66ddfd33385a212
Jason Kim <95jasonkim@gmail.com>
2020-10-09 20:32:47 -0800

    Added code for main() for user interface and fixed FifoPop()

--- HW4/hw4add2/fifo-lifo.cpp
+++ HW4/hw4add2/fifo-lifo.cpp
@@ -8,6 +8,7 @@ using std::cout;
 using std::endl;
 using std::cin;
 #include<string>
+using std::stoi;
 using std::string;
 using std::getline;
 #include<vector>
@@ -24,7 +25,7 @@ void FifoPush(vector<string>& container, const string& item)
 void FifoPop(vector<string>& container, string& item)
 {
 	item = container[0];
-	container.erase(container.begin() + 1);
+	container.erase(container.begin());
 }
 
 //item is pushed to container
@@ -59,6 +60,75 @@ void PrintContainer(const vector<string>& container)
 
 int main()
 {
-
-
+	vector<string> Fifo;
+	vector<string> Lifo;
+	while (true)
+	{
+		string input;
+		cout << "Enter \"1\" to try Fifo or enter \"2\" to try Lifo." << endl;
+		getline(cin, input);
+		int choice = stoi(input);
+		if (choice == 1)
+		{
+			while (true)
+			{
+				cout << "Type in \"push\" or \"pop\"(type in \"end\" to go back): ";
+				getline(cin, input);
+				if (input == "push")
+				{
+					cout << "Type in the word to push: ";
+					getline(cin, input);
+					FifoPush(Fifo, input);
+					cout << endl;
+				}
+				else if (input == "pop")
+				{
+					string popped;
+					if (isContainerEmpty(Fifo))
+					{
+						cout << "Fifo is already empty." << endl;
+					}
+					else
+					{
+						FifoPop(Fifo, popped);
+						cout << popped << " has been popped." << endl;
+					}
+				}
+				else if (input == "end")break;
+			}
+			cout << "The items in Fifo are: " << endl;
+			PrintContainer(Fifo);
+		}
+		else if (choice == 2)
+		{
+			while (true)
+			{
+				cout << "Type in \"push\" or \"pop\"(type in \"end\" to go back): ";
+				getline(cin, input);
+				if (input == "push")
+				{
+					cout << "Type in the word to push: ";
+					getline(cin, input);
+					LifoPush(Lifo, input);
+					cout << endl;
+				}
+				else if (input == "pop")
+				{
+					string popped;
+					if (isContainerEmpty(Lifo))
+					{
+						cout << "Lifo is already empty." << endl;
+					}
+					else
+					{
+						LifoPop(Lifo, popped);
+						cout << popped << " has been popped." << endl;
+					}
+				}
+				else if (input == "end")break;
+			}
+			cout << "The items in Lifo are: " << endl;
+			PrintContainer(Lifo);
+		}
+	}
 }
