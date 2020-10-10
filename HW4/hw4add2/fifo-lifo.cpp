89f6fac955eac1d48b85f89a4186e1ee0afc2c9d
Jason Kim <95jasonkim@gmail.com>
2020-10-09 20:02:04 -0800

    added function FifoPush()

--- HW4/hw4add2/fifo-lifo.cpp
+++ HW4/hw4add2/fifo-lifo.cpp
@@ -3,3 +3,24 @@ Jason Kim
 CS201 hw4 fifo-lifo.cpp
 10/9/2020
 */
+#include<iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include<string>
+using std::string;
+using std::getline;
+#include<vector>
+using std::vector;
+
+//pushes @param item to @param container
+void FifoPush(vector<string>& container, const string& item)
+{
+	container.push_back(item);
+}
+
+int main()
+{
+
+
+}
