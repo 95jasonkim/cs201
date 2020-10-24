f7b93c30d0d895c7fa75e9cb39aa110eb68833c3
Jason Kim <95jasonkim@gmail.com>
2020-10-23 19:55:53 -0800

    finished lab21

--- labs/lab21/lab21.cpp
+++ labs/lab21/lab21.cpp
@@ -4,3 +4,23 @@ CS201 lab21
 10/23/2020
 */
 #include "truncstruct.hpp"
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include<string>
+using std::getline;
+using std::string;
+
+int main()
+{
+	cout << "Type in some text: " << endl;
+	string s;
+	getline(cin, s);
+	cout << "Type in a number: " << endl;
+	string n;
+	getline(cin, n);
+	size_t num = std::stoi(n);
+	cout << "Text now truncated: "<<endl;
+	cout << trunc(StringInfo { s,num }).str << endl;
+}
