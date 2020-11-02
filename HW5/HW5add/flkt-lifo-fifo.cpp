95815cb0e9af59e6836357fa4930b547fffe07af
Jason Kim <95jasonkim@gmail.com>
2020-11-01 18:11:54 -0900

    created function print that updates the items in the window

--- HW5/HW5add/flkt-lifo-fifo.cpp
+++ HW5/HW5add/flkt-lifo-fifo.cpp
@@ -11,15 +11,18 @@ CS201 HW5 flkt-lifo-fifo.cpp
 #include<FL/Fl_Output.H>
 #include"lifo-fifo.hpp"
 
+
+vector<string> container{ "one","two","three","four","five","six","seven" };
+
 void exit(Fl_Widget* b, void*)
 {
 	exit(0);
 }
 
-string printed(const vector<string>& container)
+string printed(const vector<string>& con)
 {
 	string print;
-	for (auto item : container)
+	for (auto item : con)
 	{
 		print += item + ", ";
 	}
@@ -28,24 +31,31 @@ string printed(const vector<string>& container)
 	return print;
 }
 
+void print(Fl_Widget* box)
+{
+	Fl_Box* item = (Fl_Box*)box;
+	string print = printed(container);
+	const char* c = print.c_str();
+	item->copy_label(c);
+}
+
+
 int main()
 {
-	vector<string> container {"one","two","three","four","five","six","seven"};
 	Fl_Window* window=new Fl_Window(700, 500, "LiFo-FiFo");
 	window->begin();
 	Fl_Input* in = new Fl_Input(100, 250, 250, 70, "Input Item");
 	Fl_Box* intro = new Fl_Box(50, 50, 200, 50, "Items in container");
 	Fl_Box* items = new Fl_Box(50, 100, 600, 70,"place");
 	items->box(FL_DOWN_BOX);
-	string print = printed(container);
-	const char* c = print.c_str();
-	items->label(c);
 	Fl_Button* push = new Fl_Button(450, 250, 100, 70, "Push Item");
 	Fl_Button* fifoPop = new Fl_Button(100, 385, 100, 70, "Pop first Item");
 	Fl_Button* lifoPop = new Fl_Button(300, 385, 100, 70, "Pop last Item");
 	Fl_Button* quit = new Fl_Button(550, 400, 100, 55, "Quit");
+	push->callback(pushItem,items);
 	quit->callback(exit);
 	window->end();
+	print(items);
 	window->show();
 	return Fl::run();
 }
