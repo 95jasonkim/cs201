291f942587d3702e39e3e2390fa1f118e4796d22
Jason Kim <95jasonkim@gmail.com>
2020-11-01 16:50:21 -0900

    created quit button and function

--- HW5/HW5add/flkt-lifo-fifo.cpp
+++ HW5/HW5add/flkt-lifo-fifo.cpp
@@ -11,10 +11,19 @@ CS201 HW5 flkt-lifo-fifo.cpp
 #include<FL/Fl_Output.H>
 #include"lifo-fifo.hpp"
 
+void exit(Fl_Widget* b, void*)
+{
+	exit(0);
+}
+
+
 int main()
 {
-	Fl_Window* window=new Fl_Window(300, 300, "LiFo-FiFo");
+	Fl_Window* window=new Fl_Window(1000, 500, "LiFo-FiFo");
 	window->begin();
+	Fl_Button* quit = new Fl_Button(350, 200, 50, 50, "Quit");
+	quit->type(FL_NORMAL_BUTTON);
+	quit->callback(exit);
 	window->end();
 	window->show();
 	return Fl::run();
