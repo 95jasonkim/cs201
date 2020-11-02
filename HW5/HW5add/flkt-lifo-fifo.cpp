235f75da84fb81f4d53ac40dbd56466fd06c7302
Jason Kim <95jasonkim@gmail.com>
2020-11-01 17:15:32 -0900

    created widgets and set locations

--- HW5/HW5add/flkt-lifo-fifo.cpp
+++ HW5/HW5add/flkt-lifo-fifo.cpp
@@ -19,9 +19,16 @@ void exit(Fl_Widget* b, void*)
 
 int main()
 {
-	Fl_Window* window=new Fl_Window(1000, 500, "LiFo-FiFo");
+	Fl_Window* window=new Fl_Window(700, 500, "LiFo-FiFo");
 	window->begin();
-	Fl_Button* quit = new Fl_Button(350, 200, 50, 50, "Quit");
+	Fl_Input* in = new Fl_Input(100, 250, 250, 70, "Input Item");
+	Fl_Box* intro = new Fl_Box(50, 50, 200, 50, "Items in container");
+	Fl_Box* items = new Fl_Box(50, 100, 600, 70,"place");
+	items->box(FL_DOWN_BOX);
+	Fl_Button* push = new Fl_Button(450, 250, 100, 70, "Push Item");
+	Fl_Button* fifoPop = new Fl_Button(100, 385, 100, 70, "Pop first Item");
+	Fl_Button* lifoPop = new Fl_Button(300, 385, 100, 70, "Pop last Item");
+	Fl_Button* quit = new Fl_Button(550, 400, 100, 55, "Quit");
 	quit->type(FL_NORMAL_BUTTON);
 	quit->callback(exit);
 	window->end();
