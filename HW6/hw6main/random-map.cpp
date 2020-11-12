f8c0348938a07b76e6ff4bfce7609bd9f95a0391
Jason Kim <95jasonkim@gmail.com>
2020-11-11 17:13:46 -0900

    added function RandBetween that returns random number using rand()

--- HW6/hw6main/random-map.cpp
+++ HW6/hw6main/random-map.cpp
@@ -9,6 +9,11 @@ CS201 HW6 random-map
 #include<string>
 #include<cmath>
 #include<map>
+#include<cstdlib>
+#include<ctime>
+using std::cout;
+using std::endl;
+using std::map;
 using std::mt19937;
 using std::uniform_int_distribution;
 using std::normal_distribution;
@@ -16,6 +21,7 @@ using std::random_device;
 
 random_device ran;
 mt19937 gen(ran());
+
 
 int RandomBetween(int first, int last)
 {
@@ -27,15 +33,25 @@ int RandomBetweenN(int first, int last)
 {
 	int mean = (first + last) / 2; 
 	//the mean for the normal deviation is the average of the two ints
-	int deviation = (mean - first) / 3; 
+	double deviation = (mean - first) / 3; 
 	// the standard deviation is one third the distance from the mean to the edge
 	// so only 0.1% on both sides would be outside the boundary
-	normal_distribution<int> ndist(mean,deviation);
+	normal_distribution<> ndist(mean,deviation);
 	return ndist(gen);
+}
+
+int RandBetween(int first, int last)
+{
+	int range = last - first +1;
+	//range is the number of possible outcomes
+	int result = rand() % range +first;
+	// gets a random number from 0 to range -1 and adds to first
+	return result;
 }
 
 int main()
 {
+	std::srand(std::time(nullptr));
 
 }
 
