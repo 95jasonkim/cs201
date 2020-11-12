da60d07fef7eb84605fe4a6b20c9c436763db9b7
Jason Kim <95jasonkim@gmail.com>
2020-11-11 18:19:05 -0900

    added function tryGuess which returns the indices of where the guess would go

--- HW6/Hw6add1/hangman.cpp
+++ HW6/Hw6add1/hangman.cpp
@@ -8,6 +8,7 @@ CS201 Hw6 hangman
 #include<map>
 #include<algorithm>
 #include<vector>
+#include<iterator>
 using std::vector;
 using std::cout;
 using std::endl;
@@ -19,14 +20,48 @@ char getGuess()
 	cout << "Type in your letter guess!" << endl;
 	string guess;
 	std::getline(std::cin, guess);
-	return guess[0];
+	return std::toupper(guess[0]);
+}
+
+vector<int> tryGuess(const string& answer, char guess)
+{
+	vector<int> ind;
+	for (int i =0;i<answer.size();i++)
+	{
+		if (answer[i] == guess)
+		{
+			ind.push_back(i);
+		}
+	}
+	return ind;
 }
 
 int main()
 {
 	string answer = "ALASKA";
+	string guess;
+	for (auto c : answer)
+	{
+		guess += "-";
+	}
+	map<char, int> abc;
 	vector<string> chances{ "head","body","right arm","left arm",
 		"right leg","left leg","right hand","left hand","right foot","left foot" };
-	cout << "Play Hangman! You have 10 guesses." << endl;
-	
+	cout << "Play Hangman! You have 10 guesses." << '\n'<<endl;
+	cout << guess << endl;
+	while (true)
+	{
+		char g = getGuess();
+		if (abc[g] == 1)
+		{
+			cout << "You have already tried this letter! Try again." << endl;
+			continue;
+		}
+		abc[g] = 1;
+		auto ind = tryGuess(answer, g);
+		if (ind.empty())
+		{
+
+		}
+	}
 }
