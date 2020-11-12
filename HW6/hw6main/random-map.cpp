d1fc794dc5906b5a479a06354fc8cc598ec2bf21
Jason Kim <95jasonkim@gmail.com>
2020-11-11 16:37:57 -0900

    Added RandomBetween function, returns a uniform random number

--- HW6/hw6main/random-map.cpp
+++ HW6/hw6main/random-map.cpp
@@ -3,3 +3,30 @@ Jason Kim
 CS201 HW6 random-map
 11/11/2020
 */
+#include<iostream>
+#include<iomanip>
+#include<random>
+#include<string>
+#include<cmath>
+#include<map>
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::normal_distribution;
+using std::random_device;
+
+random_device ran;
+mt19937 gen(ran());
+
+int RandomBetween(int first, int last)
+{
+	uniform_int_distribution<int> dist(first, last);
+	return dist(gen);
+}
+
+
+
+int main()
+{
+
+}
+
