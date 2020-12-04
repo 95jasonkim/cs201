afe388e98d4687297046043f86b1a3e341e28a96
Jason Kim <95jasonkim@gmail.com>
2020-12-04 09:34:34 -0900

    constructor for environment class

--- HW8/Hw8 main/environment.hpp
+++ HW8/Hw8 main/environment.hpp
@@ -3,10 +3,19 @@ Jason Kim
 CS201 hw8 main
 environment.hpp
 */
-
-
 #ifndef ENVIRONMENT_HPP
 #define ENVIRONMENT_HPP
+
+class Environment
+{
+public:
+	Environment(double temperature = 20.0, bool heater=true): _temperature{temperature}, _heater{heater}
+	{}
+
+private:
+	double _temperature;
+	bool _heater;
+};
 
 
 
